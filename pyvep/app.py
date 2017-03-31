0d9c7a1420663fa37c77c4b3cf8e0baf892b95ee
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2017-03-31 19:30:47 -0400

    Init download

--- pyvep/app.py
+++ pyvep/app.py
@@ -2,14 +2,16 @@
 import flask
 from flask.ext.cors import CORS
 from pyvep.config import config
+from pyvep.vep import vep
 
 app = flask.Flask(__name__)
 CORS(app)
+init_url = config('init_url')
 base_url = config('base_url')
 
 
-@app.route(base_url, methods=['GET'])
-def hello_world():
+@app.route(init_url, methods=['GET'])
+def download_ref():
     return ""
 
 
@@ -17,7 +19,7 @@ def hello_world():
 def api():
     print(type(flask.request.get_json(force=True)))
     genelist = flask.request.get_json(force=True)
-    return flask.jsonify(get_synonyms(genelist))
+    return flask.jsonify()
 
 if __name__ == '__main__':
     app.run()
--- pyvep/vep.py
+++ pyvep/vep.py
@@ -1,9 +1,17 @@
 __author__ = 'maximkuleshov'
 
 import subprocess
+vep_dir = ''
 
+def download():
+    bash_command = 'wget -i ftp://ftp.ensembl.org/pub/release-87/variation/VEP/homo_sapiens_vep_87_GRCh38.tar.gz'
+    # RUN tar xfz homo_sapiens_vep_87_GRCh37.tar.gz
+    # RUN rm homo_sapiens_vep_87_GRCh37.tar.gz
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE, cwd=vep_dir)
+    output, error = process.communicate()
+    return None
 
-def main():
+def run():
     bash_command = './vep.pl --gencode_basic --species homo_sapiens --symbol --cache --input_file test.vcf '
     vep_dir = '/Users/maximkuleshov/Work/snpEff/'
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE, cwd=vep_dir)
@@ -13,5 +21,9 @@ def main():
     return None
 
 
-if __name__ == '__main__':
+def main():
+    return None
+
+
+if __name__ == '__vep__':
     main()
