2c4a4458028f55d03fe8e2655202ce71404aacc9
Maxim Kuleshov <kuleshov.max.v@gmail.com>
2017-04-19 16:13:12 -0400

    Fix for deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-$dockerfile='Dockerfile'
+dockerfile='Dockerfile'
 
 if [[ $1 = 'prod' ]]; then
-	refs='RUN perl INSTALL.pl -a c -s homo_sapiens -y GRCh38 -l --NO_TEST'
-	convert_cahce='RUN perl convert_cache.pl -species all -version all'
-	printf '%s\n%s' $refs $convert_cahce > $dockerfile
+    echo 'RUN perl INSTALL.pl -a c -s homo_sapiens -y GRCh38 -l --NO_TEST' >> $dockerfile
+	echo -n 'RUN perl convert_cache.pl -species all -version all'  >> $dockerfile
 fi
 
 docker build -t maayanlab/pyvep:latest .
